d076e28a9a547de56ae3c4014b7256766eb435ea
sahin-oguzhan <oguzhan.sahin99@outlook.com>
2025-09-04 02:59:03 +0300

    CategoryPick created

---
+++ ecommerce/src/components/CategoryPick.jsx
@@ -0,0 +1,28 @@
+export default function CategoryPick() {
+  return (
+    <div className="flex h-screen flex-col gap-30">
+      <div className="font-montserrat flex flex-col gap-5 pt-15 text-center">
+        <h1 className="text-text-color text-2xl font-bold">EDITOR'S PICK</h1>
+        <p className="text-second-text-color text-sm font-normal">
+          Problems trying to resolve the conflict between
+        </p>
+      </div>
+      <div className="flex justify-center gap-10">
+        <div className="h-[500px] w-[510px] bg-[url('images/category-pick-1.jpg')] bg-cover">
+          <button className="">MEN</button>
+        </div>
+        <div className="h-[500px] w-[240px] bg-[url('images/category-pick-2.jpg')] bg-cover">
+          <p>WOMEN</p>
+        </div>
+        <div className="flex flex-col gap-5">
+          <div className="h-[240px] w-[240px] bg-[url('images/category-pick-3.jpg')] bg-cover">
+            <p>ACCESSORIES</p>
+          </div>
+          <div className="h-[240px] w-[240px] bg-[url('images/category-pick-4.jpg')] bg-cover">
+            <p>KIDS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
