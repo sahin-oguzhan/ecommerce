82ffec7486f92fb66b7f27174cf4ee33fa94dfa2
sahin-oguzhan <oguzhan.sahin99@outlook.com>
2025-09-09 18:50:47 +0300

    Dropdown Menu update

--- ecommerce/src/components/NavBar/NavBar.jsx
+++ ecommerce/src/components/NavBar/NavBar.jsx
@@ -12,19 +12,14 @@ export default function NavBar() {
     <div className="font-montserrat relative font-bold">
       <div className="text-second-text-color flex flex-row gap-5">
         <h2>Home</h2>
-        <div className="relative">
-          <button
-            onClick={toggleShop}
-            className="text-text-color flex font-medium"
-          >
+        <div className="dropdown dropdown-hover">
+          <div className="text-text-color flex">
             Shop
             <ChevronDown />
-          </button>
-          {shopMenu && (
-            <div className="absolute top-full z-20 w-max shadow-lg">
-              <ShopMenu />
-            </div>
-          )}
+          </div>
+          <div className="dropdown-content">
+            <ShopMenu />
+          </div>
         </div>
         <h2>About</h2>
         <h2>Blog</h2>
